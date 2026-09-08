55e8dcd62e3693af15af966d0e9b61fab81f2256
frontender-page <164558014+frontender-page@users.noreply.github.com>
2026-09-08 14:20:53 +0300

    Add request handler for /ping endpoint

--- addon.py
+++ addon.py
@@ -1,6 +1,18 @@
 from mitmproxy import http
 import re
 
+# --- ОБРАБОТЧИК ЗАПРОСОВ ---
+def request(flow: http.HTTPFlow) -> None:
+    # Если запрос идёт к нашему домену и пути /ping
+    if "mitmproxy-render.onrender.com" in flow.request.host and flow.request.path == "/ping":
+        flow.response = http.Response.make(
+            200,
+            b"pong",
+            {"Content-Type": "text/plain"}
+        )
+        return
+
+# --- ОБРАБОТЧИК ОТВЕТОВ (подмена оценок) ---
 def response(flow: http.HTTPFlow) -> None:
     if "nz.ua" not in flow.request.pretty_host:
         return
